381670eb69bcb59a968df6b0267a745b63a86ef2
Hashs <32106478+Hashs7@users.noreply.github.com>
2018-06-06 20:58:00 +0200

    Detection collision + incrementation score

--- js/game.js
+++ js/game.js
@@ -1,207 +1,209 @@
-$(document).ready(function() 
-{
+(function($){
 
-	// var player, enemy, point, score;
-	var nbEnemies;
+    $(document).ready(function () {
+        // var player, enemy, point, score;
+        var nbEnemies;
+        var randomPosX = Math.random() * $(window).width();
+        var randomPosY = Math.random() * $(window).height();
 
-	var randomPosX = Math.random()*$(window).width();
-	var randomPosY = Math.random()*$(window).height();
 
+        //Création des éléments du jeu
+        // 	function createGameElements()
+        // 	{
 
-// //Création des éléments du jeu 
-// 	function createGameElements()
-// 	{
 
-		
-		// //Créer enmies
-		// var i;
-		// var enemy;
+        //Créer enmies
+        // var i;
+        // var enemy;
 
-		// for(i=0; i<=nbEnemies; i++) 
-		// {
-		// 	enemy = document.createElement("div");
-		// 	enemy.setAttribute("id", "enemy_" + i);
-		// 	enemy.setAttribute
+        // for(i=0; i<=nbEnemies; i++)
+        // {
+        // 	enemy = document.createElement("div");
+        // 	enemy.setAttribute("id", "enemy_" + i);
+        // 	enemy.setAttribute
 
-		// } 
+        // }
 
-		// var randomPosX = Math.random()*$(window).width();
-		// var randomPosY = Math.random()*$(window).height();
+        // var randomPosX = Math.random()*$(window).width();
+        // var randomPosY = Math.random()*$(window).height();
 
-		// console.log("enemy")
+        // console.log("enemy")
 
 
-// 		//Créer les éléments à récolter
-// 		// Créer les obstacles
+        //Créer les éléments à récolter
+        // Créer les obstacles
 
-// 	}	
+        // 	}
 
 
-// // Initialisation du jeu
-// 	function initGame ()
-// 	{
-// 		//Déclarer les variables
-// 		//Positionner le joueur à sa position initiale
-// 	}
+        // // Initialisation du jeu
+        // 	function initGame ()
+        // 	{
+        // 		//Déclarer les variables
+        // 		//Positionner le joueur à sa position initiale
+        // 	}
 
-	//Déplacer le player
-	$(window).on("keyup", function(ev) {
+        //Déplacer le player
+        $(window).keyup(function(event){
+            bougerPerso(event);
+        });
 
-		var speed = 50;
-		var currentPosX = $("#player").offset().left;
-		var currentPosY = $("#player").offset().top;
-		var newPosRight = currentPosX + speed;
-		var newPosLeft = currentPosX - speed;
-		var newPosTop = currentPosY - speed;
-		var newPosDown = currentPosY + speed;
 
-		var gameWidth = $("#gameContainer").width();
-		var gameHeight = $("#gameContainer").height();
+        afficheBonbon();
+    });
 
-		var playerWidth = $("#player").width();
-		var playerHeight = $("#player").height();
-		
 
+    var gameWidth = $("#gameContainer").width();
+    var gameHeight = $("#gameContainer").height();
 
+    var playerWidth = $("#player").width();
+    var playerHeight = $("#player").height();
 
+    // Faire bouger le perso en fonction de la touche appuyee
+    function bougerPerso(ev){
+        var speed = 50;
 
-		// alert(ev.keyCode);
+        var currentPosX = $("#player").offset().left;
+        var currentPosY = $("#player").offset().top;
 
-			if(ev.keyCode == 39) 
-			{
-				if((newPosRight + playerWidth) > gameWidth) 
-				{
-					newPosRight = gameWidth - playerWidth;
-				}
+        var newPosRight = currentPosX + speed;
+        var newPosLeft = currentPosX - speed;
+        var newPosTop = currentPosY - speed;
+        var newPosDown = currentPosY + speed;
 
-				newPosRight = $("#player").css({left: newPosRight + "px"});
-				
-			}
 
-			if(ev.keyCode == 37) {
 
-				if(newPosLeft < 0) 
-				{
-					newPosLeft = 0;
-				}
+        // Déplacer vers la droite
+        if (ev.keyCode == 39) {
+            if ((newPosRight + playerWidth) > gameWidth) {
+                newPosRight = gameWidth - playerWidth;
+            }
+            $("#player").css({left: newPosRight + "px"});
+        }
 
-				$("#player").css({left: newPosLeft + "px"});
-			}
+        // Déplacer vers la gauche
+        if (ev.keyCode == 37) {
+            if (newPosLeft < 0) {
+                newPosLeft = 0;
+            }
+            $("#player").css({left: newPosLeft + "px"});
+        }
 
-			if(ev.keyCode == 38) {
+        // Déplacer vers la haut
+        if (ev.keyCode == 38) {
+            if (newPosTop < 0) {
+                newPosTop = 0;
+            }
 
-				if(newPosTop < 0) 
-				{
-					newPosTop = 0;
-				}
+            $("#player").css({top: newPosTop + "px"});
+        }
 
-				$("#player").css({top: newPosTop + "px"});
-			}
+        // Déplacer vers la bas
+        if (ev.keyCode == 40) {
+
+            if ((newPosDown + playerWidth) > gameHeight) {
+                newPosDown = gameHeight - playerHeight;
+            }
+
+            $("#player").css({top: newPosDown + "px"});
+        }
 
-			if(ev.keyCode == 40) {
+        var offsetPlayer = $("#player").offset();
 
-				if((newPosDown + playerWidth) > gameHeight) 
-				{
-					newPosDown = gameHeight - playerHeight;
-				}
+        detectCollision(offsetPlayer);
+    }
 
-				$("#player").css({top: newPosDown + "px"});
-			}
 
-		console.log("Offset du perso : ");
-		console.log($("#player").offset());
-		console.log(gameWidth);
-		console.log(newPosTop);
+    // Affiche un bonbon aléatoirement
+    function afficheBonbon() {
+        var candyType = Math.floor(Math.random() * 3);
 
+        switch (candyType) {
+            case 0:
+                var $bonbon = $("#bonbon");
+                var bonbonWidth = $bonbon.width();
+                var bonbonHeight = $bonbon.height();
 
-	})
+                var randomPosX = Math.floor(Math.random() * ($(window).width() - bonbonWidth));
+                var randomPosY = Math.floor(Math.random() * ($(window).height() - bonbonHeight));
 
-	//faire apparêtre les bonbons
-	function afficheElements()
-	{
-		var randomPosX = Math.random()*$(window).width();
-		var randomPosY = Math.random()*$(window).height();
-		var candyType = Math.floor(Math.random()*3);
+                $bonbon.addClass("currentBonbon");
+                $bonbon.css({
+                    top: randomPosY + "px",
+                    left: randomPosX + "px"
+                });
+                break;
 
+            case 1:
+                var $bonbon = $("#coeur");
+                var bonbonWidth = $bonbon.width();
+                var bonbonHeight = $bonbon.height();
 
-		switch(candyType){
-			case 0: 
-				$("#bonbon").css({top: randomPosY + "px"})
-				$("#bonbon").css({left: randomPosX + "px"})
-				break;
-				
+                var randomPosX = Math.floor(Math.random() * ($(window).width() - bonbonWidth));
+                var randomPosY = Math.floor(Math.random() * ($(window).height() - bonbonHeight));
 
-		}
+                $bonbon.addClass("currentBonbon");
+                $bonbon.css({
+                    top: randomPosY + "px",
+                    left: randomPosX + "px"
+                });
+                break;
 
-		if(candyType == 0)
-		{
-			$("#bonbon").css({top: randomPosY + "px"})
-			$("#bonbon").css({left: randomPosX + "px"})
-		}
-		else {
-			$("#coeur").hide();
-			$("#guimauve_rose").hide();
-		}
+            case 2:
+                var $bonbon = $("#guimauve_rose");
+                var bonbonWidth = $bonbon.width();
+                var bonbonHeight = $bonbon.height();
 
-		if(candyType == 1)
-		{
-			$("#coeur").css({top: randomPosY + "px"})
-			$("#coeur").css({left: randomPosX + "px"})
-		}
-		else {
-			$("#bonbon").hide();
-		}
+                var randomPosX = Math.floor(Math.random() * ($(window).width() - bonbonWidth));
+                var randomPosY = Math.floor(Math.random() * ($(window).height() - bonbonHeight));
+                $bonbon.addClass("currentBonbon");
+                $bonbon.css({
+                    top: randomPosY + "px",
+                    left: randomPosX + "px"
+                });
+                break;
 
-		if(candyType == 2)
-		{
-			$("#guimauve_rose").css({top: randomPosY + "px"})
-			$("#guimauve_rose").css({left: randomPosX + "px"})
+            default:
+                break;
+        }
+    }
 
-			else {
-			$("#bonbon").hide();
-			$("#coeur").hide();
-			}
+    function moveEnemies (){
+        //mise à jour de la position des ennemis
+    }
 
-		}
-	}
+    function refreshGame(){
+        //Appel à la fonction moveEnnemies()
+        //Vérifier collision : appel à la fonction detectCollision
+    }
 
-	afficheElements();
+    var score = 0;
 
+    function detectCollision (positionPerso){
+        var $currentBonbon = $(".currentBonbon");
+        var bonbonWidth = $currentBonbon.width();
+        var bonbonHeight = $currentBonbon.height();
+        var positionBonbon = $currentBonbon.offset();
 
+        console.log(positionBonbon.left, positionPerso.left, positionBonbon.left + bonbonWidth);
 
+        if(positionBonbon.left <= positionPerso.left && positionPerso.left <= positionBonbon.left + bonbonWidth){
+            if(positionBonbon.top <= positionPerso.top && positionPerso.top <= positionBonbon.top + bonbonHeight){
+                // Bonbon touché
+                $currentBonbon.removeClass("currentBonbon");
+                score++;
+                $(".currentScore").html(score);
 
+                afficheBonbon();
+            }
+        }
 
 
-	
-	
-	
-	// function moveEnemies ()
-	// {
-	// 	//mise à jour de la position des ennemis 
-	// }
+    }
 
-	// function refreshGame() 
-	// {
-	// 	//Appel à la fonction moveEnnemies()
-	// 	//Vérifier collision : appel à la fonction detectCollision 
-	// }
+    function setScore (){
 
+    }
 
+})(jQuery);
 
-
-	// function detectCollision ()
-	// {
-
-	// }
-
-	// function setScore ()
-	// {
-
-	// }
-
-
-
-
-
-
-});
